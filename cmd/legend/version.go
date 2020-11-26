7f9c04058bf0b6a2083f05ed4ead39a381486ff4
Tom Bell <tomb@tomb.io>
2020-11-26 21:06:26 +0000

    Add initial legend cmd package

---
+++ cmd/legend/version.go
@@ -0,0 +1,10 @@
+package main
+
+// The values of these variables are injected at compile time from the VERSION
+// environment variable and latest Git commit.
+
+// Version is the current version.
+var version = "dev"
+
+// Commit is the git commit for the build.
+var commit string
