41d3e5742cec2a2cf234585a2f70626a09b9e870
Tom Bell <tomb@tomb.io>
2020-11-28 18:11:27 +0000

    Add fields to Deck struct

--- pkg/decks/deck.go
+++ pkg/decks/deck.go
@@ -1,5 +1,11 @@
 package decks
 
-// Deck ...
+import "github.com/tombell/go-rekordbox"
+
+// Deck is a single deck in rekordbox, which has a currently playing track, and
+// history of played tracks.
 type Deck struct {
+	ID      int
+	Current *rekordbox.Track
+	History []*rekordbox.Track
 }
