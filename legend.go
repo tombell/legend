7e84f0ab8a7e486383f3f638225caa091f797f62
Tom Bell <tomb@tomb.io>
2021-01-03 14:59:06 +0000

    Rename single letter variables

--- legend.go
+++ legend.go
@@ -37,11 +37,11 @@ func Run(options *Options) error {
 
 	errCh := make(chan error, 1)
 
-	m := monitor.New(options.Logger, db, options.Interval, playlist)
-	s := api.New(options.Logger, playlist, options.Listen)
+	mon := monitor.New(options.Logger, db, options.Interval, playlist)
+	srv := api.New(options.Logger, playlist, options.Listen)
 
-	go m.Run(errCh)
-	go s.Start(errCh)
+	go mon.Run(errCh)
+	go srv.Start(errCh)
 
 	ch := make(chan os.Signal)
 	signal.Notify(ch, os.Interrupt, syscall.SIGTERM)
@@ -59,7 +59,7 @@ func Run(options *Options) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	if err := s.Shutdown(ctx); err != nil {
+	if err := srv.Shutdown(ctx); err != nil {
 		return fmt.Errorf("server shutdown failed: %w", err)
 	}
 
